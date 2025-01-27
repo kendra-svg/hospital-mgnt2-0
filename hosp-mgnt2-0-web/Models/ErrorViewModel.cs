bd31baaf9a8671abd625870802420f1fdfb7714f
kendra-svg <ksanchov@ucenfotec.ac.cr>
2025-01-26 20:24:52 -0600

    Add project files.

---
+++ hosp-mgnt2-0-web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace hosp_mgnt2_0_web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
